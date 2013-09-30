8f60c8d721944e5c310833ee56913ed1c1f12311
Milen Bogunski <nelimbg@gmail.com>
2013-10-01 01:42:04 +0300

    Грешка

--- fim-di-oct2011.c
+++
@@ -1,9 +0,0 @@
-// --------------------------------------//
-// Uprajnenie 10 oct 2011                //
-// chisto C                              //
-// @author nelim                         //
-// --------------------------------------//
-
-#include <stdio.h>
-#include <string.h>
-
