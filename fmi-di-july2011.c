7ab3072afa8858eb608632b02386ffd5f1e40a4b
Milen Bogunski <nelimbg@gmail.com>
2013-10-07 12:14:06 +0300

    хелп баба

--- fmi-di-july2011.c
+++ fmi-di-july2011.c
@@ -7,7 +7,7 @@
 #define COMP_MAX_LEN 20
 #define MAX_FLIGHTS 500;
 
-int n; // broq bileti
+int n; // broq poleti
 
 struct date {
 	int day, month, year;
@@ -21,9 +21,16 @@ struct flights {
 	char company[COMP_MAX_LEN];
 };
 
+struct maxflights {
+	char company[COMP_MAX_LEN];
+	int count;
+};
+
 void addFlight(struct flights *flight) {
-	printf("Въведете дестинация: ");
-	scanf("%s", &flight->destination);
+	printf("Дестинация: ");
+	scanf("%s", (char*)&flight->destination);
+	printf("Компания: ");
+	scanf("%s", (char*)&flight->destination);
 };
 
 void getFlight(struct flights *flight) {
@@ -33,7 +40,11 @@ void getFlight(struct flights *flight) {
 
 int main(void) {
 	struct flights flight[500],tempflight;
+	struct maxflights maxflight[500]; // dvumeren masiv za broene na kompaniite ot kompaniite
+
+	int k = 0; // broi wywedeni kompanii
 	int i,j; //loops
+	int found = 0;
 	do {
 	printf("Въведете брой полети: ");
 	scanf("%d", &n);
@@ -41,6 +52,20 @@ int main(void) {
 
 	for(i=0;i<n;i++) {
 		addFlight(&flight[i]);
+		// sled wywejdane na noviq polet, pretyrswame masiva s kompaniite i wijdame ima li wywedeni poleti za tazi kompaniq
+		// ako ima - increase-wame count-a s edno
+		// ako nqma namereni, dobawqme nov i increase-wame k;
+		for(j=0;j<k;j++)
+			if(strcmp(flight[i].company,maxflight[j].company)==0) {
+				maxflight[j].count++;
+				found = 1;
+				break;
+			}
+		if(found==0) {
+			k++; // increase-wame k s edno i prawim zapis za posledniq
+			maxflight[k].company=(char[20])flight[i].company;
+			maxflight[k].count=1;
+		}
 	}
 
 	for(i=0;i<n;i++) {
